53441f2e38c3f1a737c986d0c5677f6b44e66ff6
maxymoo22 <maxh2815@gmail.com>
2020-09-11 12:30:43 +1200

    Settings stuff

--- AirQuality/AirQuality.ino
+++ AirQuality/AirQuality.ino
@@ -25,80 +25,80 @@ const char* root_ca = \
 "-----END CERTIFICATE-----\n";
 
 void setup() {
-  WiFi.disconnect();
-  WiFi.mode(WIFI_STA);
-  Serial.begin(9600);
+	WiFi.disconnect();
+	WiFi.mode(WIFI_STA);
+	Serial.begin(9600);
 
-  function();
+	function();
 }
 
 void function() {
-  Serial.println("Scanning for networks");
-  int16_t networks = WiFi.scanNetworks(false, true);
-  Serial.println("Scan complete\n\nNetworks within range:");
+	Serial.println("Scanning for networks");
+	int16_t networks = WiFi.scanNetworks(false, true);
+	Serial.println("Scan complete\n\nNetworks within range:");
 
-  int viableNetworks[networks] = {};
-  int index = 0;
-  
-  for(int i = 0; i < networks; i++) {
-    if(WiFi.RSSI(i) >= -70) {
-      viableNetworks[index] = i;
-      index++;
-      Serial.println(String(i + 1) + ") " + WiFi.SSID(i) + ", " + (WiFi.encryptionType(i) == WIFI_AUTH_OPEN ? "Open" : "Secure"));
-    }
-  }
+	int viableNetworks[networks] = {};
+	int index = 0;
+	
+	for(int i = 0; i < networks; i++) {
+		if(WiFi.RSSI(i) >= -70) {
+			viableNetworks[index] = i;
+			index++;
+			Serial.println(String(i + 1) + ") " + WiFi.SSID(i) + ", " + (WiFi.encryptionType(i) == WIFI_AUTH_OPEN ? "Open" : "Secure"));
+		}
+	}
 
-  label1:
-  Serial.println("\nPlease type the corresponding number to choose the network:");
-  while(Serial.available() <= 0) {}
-  int choice = Serial.parseInt();
+	label1:
+	Serial.println("\nPlease type the corresponding number to choose the network:");
+	while(Serial.available() <= 0) {}
+	int choice = Serial.parseInt();
 
-  // parseInt returns 0 when it times out so thats ultra handy!!! It will be caught by this statement id the user enters a non int
-  if(choice < 1 or choice > index) {
-    Serial.println("\nThat wasn't a valid choice!");
-    goto label1;
-  }
+	// parseInt returns 0 when it times out so thats ultra handy!!! It will be caught by this statement id the user enters a non int
+	if(choice < 1 or choice > index) {
+		Serial.println("\nThat wasn't a valid choice!");
+		goto label1;
+	}
 
-  // Get the number of the network from our choice.
-  choice = viableNetworks[choice - 1];
+	// Get the number of the network from our choice.
+	choice = viableNetworks[choice - 1];
 
-  if(WiFi.encryptionType(choice) != WIFI_AUTH_OPEN) {
-    label2:
-    Serial.println("\nThis network requires a password. Please enter it now:");
-    while(Serial.available() <= 0) {}
-    String password = Serial.readString();
+	if(WiFi.encryptionType(choice) != WIFI_AUTH_OPEN) {
+		label2:
+		Serial.println("\nThis network requires a password. Please enter it now:");
+		while(Serial.available() <= 0) {}
+		String password = Serial.readString();
 
-    if(WiFi.begin(WiFi.SSID(choice).c_str(), password.c_str()) != WL_CONNECTED) {
-      Serial.print("\nSomething went wrong. You probably entered an incorrect password. Please try again.");
-      goto label2;
-    }
-  }
-  else {
-    label3:
-    if(WiFi.begin(WiFi.SSID(choice).c_str()) != WL_CONNECTED) {
-      Serial.println("\nSomething went wrong. Attempting to reconnect...");
-      goto label3;
-    }
-  }
+		if(WiFi.begin(WiFi.SSID(choice).c_str(), password.c_str()) != WL_CONNECTED) {
+			Serial.print("\nSomething went wrong. You probably entered an incorrect password. Please try again.");
+			goto label2;
+		}
+	}
+	else {
+		label3:
+		if(WiFi.begin(WiFi.SSID(choice).c_str()) != WL_CONNECTED) {
+			Serial.println("\nSomething went wrong. Attempting to reconnect...");
+			goto label3;
+		}
+	}
 
-  Serial.println("\nSuccessfully connected to the network!\nSending request to howsmyssl.com\n");
+	Serial.println("\nSuccessfully connected to the network!\nSending request to howsmyssl.com\n");
 
-  HTTPClient http;
+	HTTPClient http;
  
-    http.begin("https://www.howsmyssl.com/a/check", root_ca); //Specify the URL and certificate
-    int httpCode = http.GET();
+		http.begin("https://www.howsmyssl.com/a/check", root_ca); //Specify the URL and certificate
+		int httpCode = http.GET();
  
-    if (httpCode > 0) { //Check for the returning code
-        String payload = http.getString();
-        Serial.println(httpCode);
-        Serial.println(payload);
-      }
+		if (httpCode > 0) { //Check for the returning code
+				String payload = http.getString();
+				Serial.println(httpCode);
+				Serial.println(payload);
+			}
  
-    else {
-      Serial.println("Error on HTTP request");
-    }
+		else {
+			Serial.println("Error on HTTP request");
+		}
  
-    http.end(); //Free the resources
+		http.end(); //Free the resources
 }
 
 void loop() {}*/
@@ -121,99 +121,113 @@ WiFiServer TCPServer(1305);
 unsigned long startMillis = 0;
 
 void setup(){
-    Serial.begin(9600);
-    
-    //Connect to the WiFi network
-    WiFi.begin("Hosking Family Wifi 2.4GHz", "resapujafe");
-    Serial.println("");
+		Serial.begin(9600);
+		
+		// Connect to the WiFi network
+		WiFi.begin("Hosking Family Wifi 2.4GHz", "resapujafe");
+		Serial.println("");
 
-    // Wait for connection
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(250);
-      Serial.print(".");
-    }
-    
-    Serial.println("");
-    Serial.print("Connected to ");
-    Serial.println(ssid);
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-    
-    //This initializes udp
-    udp.begin(WiFi.localIP(), 1304);
+		// Wait for connection
+		while (WiFi.status() != WL_CONNECTED) {
+			delay(250);
+			Serial.print(".");
+		}
+		
+		Serial.println("");
+		Serial.print("Connected to wifi");
+		//Serial.println(ssid);
+		Serial.print("IP address: ");
+		Serial.println(WiFi.localIP());
+		
+		//This initializes udp
+		udp.begin(WiFi.localIP(), 1304);
 
-    // Start the socket server for when we want to send data to clients
-    TCPServer.begin();
+		// Start the socket server for when we want to send data to clients
+		TCPServer.begin();
 }
 
 void loop() {
-  WiFiClient incomingClient = TCPServer.available();
-  // If we have an incoming client and they're sending us data, its the app on the users phone. We only want a maximum of 5 clients
-  if(incomingClient && clientCount < 5) {
-    // Store the client
-    clients[clientCount] = incomingClient;
-    Serial.println("Client connected. Client number: " + String(clientCount));
-    clientCount++;
-  }
+	WiFiClient incomingClient = TCPServer.available();
+	// If we have an incoming client and they're sending us data, its the app on the users phone. We only want a maximum of 5 clients
+	if(incomingClient && clientCount < 5) {
+		bool clientAlreadyConnected = false;
+		for(uint8_t i = 0; i < clientCount; i++) {
+			if(clients[i] == incomingClient) {
+				clientAlreadyConnected = true;
+				break;
+			}
+		}
+		
+		// Store the client if we dont have them already
+		if(!clientAlreadyConnected) {
+			clients[clientCount] = incomingClient;
+			Serial.println("Client connected. Client number: " + String(clientCount));
+			clientCount++;
+		}
+		// Otherwise the app will be sending us a password for the wifi
+		else {
+			// Do stuff
+		}
+	}
 
-  // If a second has passed then we need to update the sensor data
-  if(millis() - startMillis >= 1000) {
-    for(byte i = 0; i < clientCount; i++) {
-      // If they aren't connected anymore then we can remove them and move on to the next client
-      if(clients[i].connected() == false) {
-        Serial.println("Client " + String(i) + " disconnected");
-        clients[i].stop();
-        removeClient(i);
-        continue;
-      }
+	// If a second has passed then we need to update the sensor data
+	if(millis() - startMillis >= 1000) {
+		for(byte i = 0; i < clientCount; i++) {
+			// If they aren't connected anymore then we can remove them and move on to the next client
+			if(clients[i].connected() == false) {
+				Serial.println("Client " + String(i) + " disconnected");
+				clients[i].stop();
+				removeClient(i);
+				continue;
+			}
 
-      // Read sensors
-      int value1 = analogRead(36);
-      int value2 = analogRead(39);
-      // Send the data to the app
-      clients[i].print(String(value1) + "," + String(value2));
-    }
+			// Read sensors
+			int value1 = analogRead(36);
+			int value2 = analogRead(39);
+			// Send the data to the app
+			clients[i].print(String(value1) + "," + String(value2));
+		}
 
-    startMillis = millis();
-  }
+		startMillis = millis();
+	}
 
-  // We can still listen for broadcasts and respond to them
-  broadcastListener();
+	// We can still listen for broadcasts and respond to them
+	broadcastListener();
 }
 
 // Small util function
 void removeClient(byte index) {
-  // Move every client down by one in the clients array
-  for(byte i = index; i < clientCount - 1; i++) {
-    clients[i] = clients[i + 1];
-  }
-  // We now have one less client
-  clientCount--;
+	// Move every client down by one in the clients array
+	for(byte i = index; i < clientCount - 1; i++) {
+		clients[i] = clients[i + 1];
+	}
+	// We now have one less client
+	clientCount--;
 }
 
 // Function to receive UDP broadcasts from clients who want to connect
 void broadcastListener() {
-  // if there's data available, read a packet
-  int packetSize = udp.parsePacket();
-  if (packetSize) {
-    
-    // Buffer to hold incoming packet
-    char packetBuffer[255];
+	// if there's data available, read a packet
+	int packetSize = udp.parsePacket();
+	if (packetSize) {
+		
+		// Buffer to hold incoming packet
+		char packetBuffer[255];
 
-    // read the packet into packetBufffer
-    int len = udp.read(packetBuffer, 16);
-    
-    Serial.print("Received packet. Contents: ");
-    Serial.println(packetBuffer);
+		// read the packet into packetBufffer
+		int len = udp.read(packetBuffer, 16);
+		
+		Serial.print("Received packet. Contents: ");
+		Serial.println(packetBuffer);
 
-    // We don't want to reply to a random packet
-    if(strcmp(packetBuffer, "areYouTheArduino")) return;
-    
-    // We now need to send a reply to the IP address and port that sent us the packet. We can begin the packet with this call
-    udp.beginPacket(udp.remoteIP(), 1304);
-    // The reply that we are sending
-    udp.print("iAmAnArduino");
-    // endPacket sends the packet to the client
-    udp.endPacket();
-  }
+		// We don't want to reply to a random packet
+		if(strcmp(packetBuffer, "areYouTheArduino")) return;
+		
+		// We now need to send a reply to the IP address and port that sent us the packet. We can begin the packet with this call
+		udp.beginPacket(udp.remoteIP(), 1304);
+		// The reply that we are sending
+		udp.print("iAmAnArduino");
+		// endPacket sends the packet to the client
+		udp.endPacket();
+	}
 }
